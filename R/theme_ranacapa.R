9be9d083c6a699f4b6ff5e4673fe0cd4fe1db207
Gaurav <gaurav.kandlikar@gmail.com>
2018-02-05 12:46:16 -0800

    adding theme_rancapa function

---
+++ R/theme_ranacapa.R
@@ -0,0 +1,5 @@
+#' Default theme for plots
+theme_ranacapa <- function() {
+  theme(panel.grid.minor.y=element_blank(),panel.grid.minor.x=element_blank(),
+        panel.grid.major.y=element_blank(),panel.grid.major.x=element_blank())
+}
