c3b864f3e5d509c4602a5c16311ffedb0c5b8e5f
Gaurav <gaurav.kandlikar@gmail.com>
2018-02-06 14:44:18 -0800

    tweaking convert_ranacapa_to_phyloseq

--- R/anacapa_utils.R
+++ R/anacapa_utils.R
@@ -19,6 +19,13 @@ convert_anacapa_to_phyloseq <- function(ana_out, mapping_file) {
     column_to_rownames("sum.taxonomy") %>% as.matrix
   ana_out2 <- ana_out2[ , order(colnames(ana_out2))]
 
+  if (any((colSums(ana_out2 == 0)))) {
+    print("excluding column", which(colSums(ana_out2 == 0)), "because it has no taxa")
+  }
+  ana_out2 <- ana_out2[,-which(colSums(ana_out2 == 0))]
+
+  # Figure out a way to make this exclude 0 columns
+
   ana_out_physeq <- otu_table(ana_out2, taxa_are_rows = TRUE)
 
   taxon_names <- colsplit(rownames(ana_out2), ";",
