4315f78ba74d09ed6df65d758db2cdb6bf3ac77a
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2018-05-02 16:21:32 +0200

    redid documentation of valdator

--- R/input_file_validators.R
+++ R/input_file_validators.R
@@ -22,34 +22,33 @@ scrub_seqNum_column <- function(ana_taxon_table) {
 #' 3. The "xxx_seq_number" column, if it ever existed, is removed
 #' 4. All columns apart from sum.taxonomy should be numeric
 #' 5. All columns apart from sum.taxonomy should have corresponding row in metadata file
-#'
 #' @param ana_taxon_table OTU table from Anacapa
 #' @param metadata_file Qiime-style mapping
 #' @author Gaurav Kandlikar
 #' @export
 validate_input_files <- function(ana_taxon_table, metadata_file) {
 
-  #' 1. Column names exist.
+  # 1. Column names exist.
   if(is.null(colnames(ana_taxon_table))) {
     stop("The input biom table should have column names. The taxonomy column should be named 'sum.taxonomy'; the rest of the columns should be named according to their sample names.")
   }
 
-  #' 2. One of the columns is named "sum.taxonomy"
+  # 2. One of the columns is named "sum.taxonomy"
   if(!("sum.taxonomy" %in% colnames(ana_taxon_table))) {
     stop("Please make sure that the taxonomy column in the input biom table is called 'sum.taxonomy'!")
   }
 
-  #' 3. The "xxx_seq_number" column, if it ever existed, is removed
+  # 3. The "xxx_seq_number" column, if it ever existed, is removed
   if(any(stringr::str_detect(colnames(ana_taxon_table), "seq_number"))) {
     stop("Please makes sure that you have removed the 'xxx_seq_number' column from the dataset (note: this can be done with the function `scrub_seqNum_column`)")
   }
 
-  #' 4. All columns apart from sum.taxonomy should be numeric
+  # 4. All columns apart from sum.taxonomy should be numeric
   if(!(all(sapply(ana_taxon_table %>% dplyr::select(-sum.taxonomy), is.numeric)))) {
     stop("Please make sure that all columns apart from sum.taxonomy only contain numeric data!")
   }
 
-  #' 5. All columns apart from sum.taxonomy should have corresponding row in metadata file
+  # 5. All columns apart from sum.taxonomy should have corresponding row in metadata file
   if(!(all(colnames(ana_taxon_table %>% dplyr::select(-sum.taxonomy)) %in% metadata_file[,1]))) {
     stop("Please make sure that each sample in your biom table has a corresponding row in the mapping file!")
   }
